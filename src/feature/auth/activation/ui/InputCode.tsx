7b8b3addf3071df48588e3c975c2919b153b45fe
INDOCYBER ALFI ARISANDI <alfi.arisandi@ASURANSIJASINDO.CO.ID>
2024-04-25 16:25:57 +0700

    initial commit signup, signin, activate code, dashboard

--- src/feature/auth/activation/ui/InputCode.tsx
+++ src/feature/auth/activation/ui/InputCode.tsx
@@ -18,81 +18,82 @@ const InputValue = ({ ref, onChange, onKeyUp }: any) => (
 );
 
 export default function InputCode() {
-  const handler = useInputHandler();
+  // const handler = useInputHandler();
   return (
-    <div className="flex justify-center gap-3">
-      <Input
-        ref={handler.boxReference}
-        onChange={(e) => handler.handleChange(e, 0)}
-        // onKeyUp={onKeyUp}
-        maxLength={1}
-        crossOrigin={""}
-        className="!w-12 !h-12 text-center text-xl !border !border-white bg-black text-white shadow-lg shadow-white/5 ring-4 ring-transparent  focus:!border-white focus:!border-t-white focus:ring-white/20"
-        labelProps={{
-          className: "hidden",
-        }}
-        containerProps={{ className: "min-w-[0px]" }}
-      />
-      <Input
-        ref={handler.boxReference1}
-        onChange={(e) => handler.handleChange(e, 1)}
-        // onKeyUp={onKeyUp}
-        maxLength={1}
-        crossOrigin={""}
-        className="!w-12 !h-12 text-center text-xl !border !border-white bg-black text-white shadow-lg shadow-white/5 ring-4 ring-transparent  focus:!border-white focus:!border-t-white focus:ring-white/20"
-        labelProps={{
-          className: "hidden",
-        }}
-        containerProps={{ className: "min-w-[0px]" }}
-      />
-      <Input
-        ref={handler.boxReference2}
-        onChange={(e) => handler.handleChange(e, 2)}
-        // onKeyUp={onKeyUp}
-        maxLength={1}
-        crossOrigin={""}
-        className="!w-12 !h-12 text-center text-xl !border !border-white bg-black text-white shadow-lg shadow-white/5 ring-4 ring-transparent  focus:!border-white focus:!border-t-white focus:ring-white/20"
-        labelProps={{
-          className: "hidden",
-        }}
-        containerProps={{ className: "min-w-[0px]" }}
-      />
-      <Input
-        ref={handler.boxReference3}
-        onChange={(e) => handler.handleChange(e, 3)}
-        // onKeyUp={onKeyUp}
-        maxLength={1}
-        crossOrigin={""}
-        className="!w-12 !h-12 text-center text-xl !border !border-white bg-black text-white shadow-lg shadow-white/5 ring-4 ring-transparent  focus:!border-white focus:!border-t-white focus:ring-white/20"
-        labelProps={{
-          className: "hidden",
-        }}
-        containerProps={{ className: "min-w-[0px]" }}
-      />
-      <Input
-        ref={handler.boxReference4}
-        onChange={(e) => handler.handleChange(e, 4)}
-        // onKeyUp={onKeyUp}
-        maxLength={1}
-        crossOrigin={""}
-        className="!w-12 !h-12 text-center text-xl !border !border-white bg-black text-white shadow-lg shadow-white/5 ring-4 ring-transparent  focus:!border-white focus:!border-t-white focus:ring-white/20"
-        labelProps={{
-          className: "hidden",
-        }}
-        containerProps={{ className: "min-w-[0px]" }}
-      />
-      <Input
-        ref={handler.boxReference5}
-        onChange={(e) => handler.handleChange(e, 5)}
-        // onKeyUp={onKeyUp}
-        maxLength={1}
-        crossOrigin={""}
-        className="!w-12 !h-12 text-center text-xl !border !border-white bg-black text-white shadow-lg shadow-white/5 ring-4 ring-transparent  focus:!border-white focus:!border-t-white focus:ring-white/20"
-        labelProps={{
-          className: "hidden",
-        }}
-        containerProps={{ className: "min-w-[0px]" }}
-      />
-    </div>
+    <></>
+    // <div className="flex justify-center gap-3">
+    //   <Input
+    //     ref={handler.boxReference}
+    //     onChange={(e) => handler.handleChange(e, 0)}
+    //     // onKeyUp={onKeyUp}
+    //     maxLength={1}
+    //     crossOrigin={""}
+    //     className="!w-12 !h-12 text-center text-xl !border !border-white bg-black text-white shadow-lg shadow-white/5 ring-4 ring-transparent  focus:!border-white focus:!border-t-white focus:ring-white/20"
+    //     labelProps={{
+    //       className: "hidden",
+    //     }}
+    //     containerProps={{ className: "min-w-[0px]" }}
+    //   />
+    //   <Input
+    //     ref={handler.boxReference1}
+    //     onChange={(e) => handler.handleChange(e, 1)}
+    //     // onKeyUp={onKeyUp}
+    //     maxLength={1}
+    //     crossOrigin={""}
+    //     className="!w-12 !h-12 text-center text-xl !border !border-white bg-black text-white shadow-lg shadow-white/5 ring-4 ring-transparent  focus:!border-white focus:!border-t-white focus:ring-white/20"
+    //     labelProps={{
+    //       className: "hidden",
+    //     }}
+    //     containerProps={{ className: "min-w-[0px]" }}
+    //   />
+    //   <Input
+    //     ref={handler.boxReference2}
+    //     onChange={(e) => handler.handleChange(e, 2)}
+    //     // onKeyUp={onKeyUp}
+    //     maxLength={1}
+    //     crossOrigin={""}
+    //     className="!w-12 !h-12 text-center text-xl !border !border-white bg-black text-white shadow-lg shadow-white/5 ring-4 ring-transparent  focus:!border-white focus:!border-t-white focus:ring-white/20"
+    //     labelProps={{
+    //       className: "hidden",
+    //     }}
+    //     containerProps={{ className: "min-w-[0px]" }}
+    //   />
+    //   <Input
+    //     ref={handler.boxReference3}
+    //     onChange={(e) => handler.handleChange(e, 3)}
+    //     // onKeyUp={onKeyUp}
+    //     maxLength={1}
+    //     crossOrigin={""}
+    //     className="!w-12 !h-12 text-center text-xl !border !border-white bg-black text-white shadow-lg shadow-white/5 ring-4 ring-transparent  focus:!border-white focus:!border-t-white focus:ring-white/20"
+    //     labelProps={{
+    //       className: "hidden",
+    //     }}
+    //     containerProps={{ className: "min-w-[0px]" }}
+    //   />
+    //   <Input
+    //     ref={handler.boxReference4}
+    //     onChange={(e) => handler.handleChange(e, 4)}
+    //     // onKeyUp={onKeyUp}
+    //     maxLength={1}
+    //     crossOrigin={""}
+    //     className="!w-12 !h-12 text-center text-xl !border !border-white bg-black text-white shadow-lg shadow-white/5 ring-4 ring-transparent  focus:!border-white focus:!border-t-white focus:ring-white/20"
+    //     labelProps={{
+    //       className: "hidden",
+    //     }}
+    //     containerProps={{ className: "min-w-[0px]" }}
+    //   />
+    //   <Input
+    //     ref={handler.boxReference5}
+    //     onChange={(e) => handler.handleChange(e, 5)}
+    //     // onKeyUp={onKeyUp}
+    //     maxLength={1}
+    //     crossOrigin={""}
+    //     className="!w-12 !h-12 text-center text-xl !border !border-white bg-black text-white shadow-lg shadow-white/5 ring-4 ring-transparent  focus:!border-white focus:!border-t-white focus:ring-white/20"
+    //     labelProps={{
+    //       className: "hidden",
+    //     }}
+    //     containerProps={{ className: "min-w-[0px]" }}
+    //   />
+    // </div>
   );
 }
